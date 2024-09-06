3d582e7c32bddb49baabcd5bf414e8d69d21f0f5
YusufOzmen01 <yusufmarquina@gmail.com>
2024-09-06 20:02:34 +0300

    Quickstart UI updates

---
+++ app/components/Scroll.jsx
@@ -0,0 +1,25 @@
+"use client";
+
+import "../style/carousel.css"
+
+export default function Scroll() {
+    return (
+        <div className="carousel-container w-max">
+            <div className="text-3xl text-center text-black">
+                {[
+                    "Choose your language",
+                    "言語を選択してください",
+                    "选择语言",
+                    "언어를 선택하세요",
+                    "Dilinizi seçin",
+                    "Choose your language",
+
+                ].map((item, index) => (
+                    <div key={index} className="carousel-item">
+                        {item}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
