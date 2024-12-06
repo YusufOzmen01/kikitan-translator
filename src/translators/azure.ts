e6e9253fe22a4beb5f229c3fc909c03369a66f2a
YusufOzmen01 <yusufmarquina@gmail.com>
2024-12-06 20:54:15 +0300

    Implemented the logic for disabling Kikitan when muted in game

--- src/translators/azure.ts
+++
@@ -1,9 +0,0 @@
-export default async function translateAZ(text: string, source: string, target: string) {
-    const res = await fetch(`https://kikitan-translator.onrender.com/translate/azure?text=${text}&src=${source}&target=${target}`)
-
-    if (res.status != 200) {
-        throw new Error("Failed to translate")
-    }
-
-    return (await res.json()).text
-}
