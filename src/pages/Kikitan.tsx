10cc7311b915f498ee13b1db577a9d790e8bf4e7
YusufOzmen01 <yusufmarquina@gmail.com>
2024-12-06 19:49:51 +0300

    Added a proper start/stop button and updated some inconsistencies

--- src/pages/Kikitan.tsx
+++ src/pages/Kikitan.tsx
@@ -7,7 +7,10 @@ import {
     GitHub as GitHubIcon,
     SwapHoriz as SwapHorizIcon,
     Favorite as FavoriteIcon,
-    KeyboardVoice as KeyboardVoiceIcon
+    KeyboardVoice as KeyboardVoiceIcon,
+    PlayArrow as PlayArrowIcon,
+    Pause as PauseIcon
+
 } from '@mui/icons-material';
 
 import { invoke } from '@tauri-apps/api/core'
@@ -22,13 +25,11 @@ import { WebSpeech } from "../recognizers/WebSpeech";
 import translateAZ from "../translators/azure";
 import translateGT from '../translators/google_translate';
 import translateGPT from "../translators/chatgpt";
+import { localization } from "../util/localization";
 
 type KikitanProps = {
-    ovr: boolean;
-    vrc: boolean;
     config: Config;
     setConfig: (config: Config) => void;
-    ws: WebSocket | null;
     lang: Lang;
 }
 
@@ -38,13 +39,14 @@ let lock = false
 
 const translators = [translateGT, translateAZ, translateGPT]
 
-export default function Kikitan({ ovr, vrc, config, setConfig, ws, lang }: KikitanProps) {
+export default function Kikitan({ config, setConfig, lang }: KikitanProps) {
     const [detecting, setDetecting] = React.useState(true)
+    const [srStatus, setSRStatus] = React.useState(true)
 
     const [detection, setDetection] = React.useState("")
     const [translated, setTranslated] = React.useState("")
-    
-    const [defaultMicrophone, setDefaultMicrophone] = React.useState("")
+
+    const [defaultMicrophone, setDefaultMicrophone] = React.useState(localization.waiting_for_mic_access[lang])
     const [lastDefaultMicrophone, setLastDefaultMicrophone] = React.useState("")
 
     const [triggerUpdate, setTriggerUpdate] = React.useState(false)
@@ -52,26 +54,35 @@ export default function Kikitan({ ovr, vrc, config, setConfig, ws, lang }: Kikit
     const [sourceLanguage, setSourceLanguage] = React.useState(config.source_language)
     const [targetLanguage, setTargetLanguage] = React.useState(config.target_language)
 
-    React.useEffect(() => { 
+    React.useEffect(() => {
         if (sr) {
             sr.set_lang(sourceLanguage)
         }
     }, [sourceLanguage, targetLanguage])
 
     React.useEffect(() => {
+        if (sr == null) {
+            return
+        }
+
+        if (srStatus) sr.start()
+        else sr.stop()
+    }, [srStatus])
+
+    React.useEffect(() => {
         (async () => {
             if (detectionQueue.length == 0 || lock) return;
 
             console.log(config.language_settings.english_gender_change_gender)
-    
+
             const val = detectionQueue[0].replace(/%/g, "%25")
             detectionQueue = detectionQueue.slice(1)
-    
+
             lock = true
-    
+
             invoke("send_typing", { address: config.vrchat_settings.osc_address, port: `${config.vrchat_settings.osc_port}` })
             let count = 3;
-    
+
             while (count > 0) {
                 try {
                     let text = ""
@@ -106,7 +117,7 @@ export default function Kikitan({ ovr, vrc, config, setConfig, ws, lang }: Kikit
 
                 break;
             }
-    
+
             lock = false
         })();
 
@@ -119,21 +130,21 @@ export default function Kikitan({ ovr, vrc, config, setConfig, ws, lang }: Kikit
         if (sr == null) {
             setInterval(() => {
                 navigator.mediaDevices.enumerateDevices()
-                .then(function (devices) {
-                    let def = devices.filter((device) => device.kind == "audioinput")[0].label
-                    def = def.split("(")[1].split(")")[0]
-    
-                    setDefaultMicrophone(def)
-                }).catch(function (err) {
-                    console.log(err.name + ": " + err.message);
-                });
+                    .then(function (devices) {
+                        let def = devices.filter((device) => device.kind == "audioinput")[0].label
+                        def = def.split("(")[1].split(")")[0]
+
+                        setDefaultMicrophone(def)
+                    }).catch(function (err) {
+                        console.log(err.name + ": " + err.message);
+                    });
             }, 1000)
 
             sr = new WebSpeech(sourceLanguage)
 
             sr.onResult((result: string, isFinal: boolean) => {
                 if (config.mode == 1 || config.vrchat_settings.send_typing_while_talking) invoke("send_typing", { address: config.vrchat_settings.osc_address, port: `${config.vrchat_settings.osc_port}` })
-    
+
                 setDetection(result)
                 setDetecting(!isFinal)
             })
@@ -143,7 +154,7 @@ export default function Kikitan({ ovr, vrc, config, setConfig, ws, lang }: Kikit
     }, [])
 
     React.useEffect(() => {
-        if (defaultMicrophone == "") return;
+        if (defaultMicrophone == localization.waiting_for_mic_access[lang]) return;
         console.log("Default microphone is not empty")
 
         if (lastDefaultMicrophone == "") {
@@ -161,28 +172,20 @@ export default function Kikitan({ ovr, vrc, config, setConfig, ws, lang }: Kikit
 
     React.useEffect(() => {
         if (!detecting && detection.length != 0) {
-            if (ovr) {
-                if (ws != null) ws.send(`send-${sourceLanguage == "ja" ? detection.replace(/？/g, "") : detection}`)
+            if (config.mode == 0) {
+                detectionQueue = [...detectionQueue, (sourceLanguage == "ja" && config.language_settings.japanese_omit_questionmark) ? detection.replace(/？/g, "") : detection]
 
-                return;
+                return
             }
 
-            if (vrc) {
-                if (config.mode == 0) {
-                    detectionQueue = [...detectionQueue, (sourceLanguage == "ja" && config.language_settings.japanese_omit_questionmark) ? detection.replace(/？/g, "") : detection]
-
-                    return
-                }
-
-                invoke("send_message", { address: config.vrchat_settings.osc_address, port: `${config.vrchat_settings.osc_port}`, msg: (sourceLanguage == "ja" && config.language_settings.japanese_omit_questionmark) ? detection.replace(/？/g, "") : detection })
-            }
+            invoke("send_message", { address: config.vrchat_settings.osc_address, port: `${config.vrchat_settings.osc_port}`, msg: (sourceLanguage == "ja" && config.language_settings.japanese_omit_questionmark) ? detection.replace(/？/g, "") : detection })
         }
     }, [detecting, detection])
 
     return <>
         <div className="flex align-middle">
             <div>
-                <div className={`mr-16 w-96 h-48 outline outline-2 outline-slate-400 rounded-md font-bold text-center ${detecting ? "text-slate-700 italic" : "text-black"}`}>
+                <div className={`mr-16 w-96 h-48 outline outline-2 transition-all outline-slate-400 rounded-md font-bold text-center ${detecting ? "text-slate-700 italic" : "text-black"} ${srStatus ? "" : "bg-gray-400"}`}>
                     <p className="align-middle">{detection}</p>
                 </div>
                 <div className="flex">
@@ -211,7 +214,7 @@ export default function Kikitan({ ovr, vrc, config, setConfig, ws, lang }: Kikit
             </div>
 
             <div>
-                <div className="w-96 h-48 outline outline-2 outline-slate-400 rounded-md text-black font-bold text-center">
+                <div className={`w-96 h-48 outline outline-2 transition-all outline-slate-400 rounded-md text-black font-bold text-center ${srStatus ? "" : "bg-gray-400"}`}>
                     <p className="align-middle">{translated}</p>
                 </div>
                 <div>
@@ -227,13 +230,18 @@ export default function Kikitan({ ovr, vrc, config, setConfig, ws, lang }: Kikit
                 </div>
             </div>
         </div>
-        <div className="align-middle mt-2">
-            <KeyboardVoiceIcon fontSize="small" /><a className=" text-blue-700"  href="" onClick={(e) => {
+        <div className="mt-2 mb-2">
+            <Button variant="outlined" size="medium" color={srStatus ? "error" : "success"} onClick={() => { setSRStatus(!srStatus) }}><p>{!srStatus ? localization.start[lang] : localization.stop[lang]}</p> {srStatus ? <PauseIcon fontSize="small" /> : <PlayArrowIcon fontSize="small" />}</Button>
+        </div>
+        <div>
+            <KeyboardVoiceIcon fontSize="small" /><a className=" text-blue-700" href="" onClick={(e) => {
                 e.preventDefault()
 
                 invoke("show_windows_audio_settings")
             }}>{defaultMicrophone}</a>
-            <div className="mt-8 flex space-x-2">
+        </div>
+        <div className="align-middle">
+            <div className="mt-2 flex space-x-2">
                 <Button variant="contained" size="small" className="h-8" onClick={() => { open("https://twitter.com/marquina_osu") }}><XIcon fontSize="small" /></Button>
                 <Button variant="contained" size="small" className="h-8" onClick={() => { open("https://buymeacoffee.com/sergiomarquina") }}><FavoriteIcon fontSize="small" /></Button>
                 <Button variant="contained" size="small" className="h-8" onClick={() => { open("https://github.com/YusufOzmen01/kikitan-translator") }}><GitHubIcon fontSize="small" /></Button>
