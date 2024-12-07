fe0cc39ca86b06599878acab5311d96835b1f0db
YusufOzmen01 <yusufmarquina@gmail.com>
2024-12-07 23:11:44 +0300

    Updated script and README.md

--- deploy.py
+++ deploy.py
@@ -22,9 +22,9 @@ print("Creating release...")
 new_release = repo.create_git_tag_and_release(tauri_conf["version"], tauri_conf["version"], "Kikitan Translator v" + tauri_conf["version"], open("public/changelogs/en.md").read(), repo.get_commits()[0].sha, "commit")
 
 print("Uploading setup...")
-new_release.upload_asset(f"src-tauri/target/release/bundle/nsis/Kikitan Translator_{tauri_conf["version"]}_x64-setup.exe", "Kikitan Translator_x64-setup.exe")
+new_release.upload_asset("src-tauri/target/release/bundle/nsis/Kikitan Translator_{}_x64-setup.exe".format(tauri_conf["version"]), "Kikitan Translator_x64-setup.exe")
 print("Uploading update zip...")
-new_release.upload_asset(f"src-tauri/target/release/bundle/nsis/Kikitan Translator_{tauri_conf["version"]}_x64-setup.nsis.zip", "Kikitan Translator_x64-setup.nsis.zip")
+new_release.upload_asset("src-tauri/target/release/bundle/nsis/Kikitan Translator_{}_x64-setup.nsis.zip".format(tauri_conf["version"]), "Kikitan Translator_x64-setup.nsis.zip")
 
 print("Updating release...")
 new_release.update_release(make_latest="true", name=new_release.title, message=new_release.body)
@@ -34,8 +34,8 @@ update_str = json.dumps({
     "version": "v" + tauri_conf["version"],
     "platforms": {
         "windows-x86_64": {
-            "signature": open(f"src-tauri/target/release/bundle/nsis/Kikitan Translator_{tauri_conf["version"]}_x64-setup.nsis.zip.sig", "r").read(),
-            "url": f"https://github.com/YusufOzmen01/kikitan-translator/releases/download/{tauri_conf["version"]}/Kikitan.Translator_{tauri_conf["version"]}_x64-setup.nsis.zip"
+            "signature": open("src-tauri/target/release/bundle/nsis/Kikitan Translator_{}_x64-setup.nsis.zip.sig".format(tauri_conf["version"]), "r").read(),
+            "url": "https://github.com/YusufOzmen01/kikitan-translator/releases/download/{}/Kikitan.Translator_{}_x64-setup.nsis.zip".format(tauri_conf["version"], tauri_conf["version"])
         }
     }
 })
