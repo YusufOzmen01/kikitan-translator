493536206930921211319ec53729a6f0d1e76fc9
YusufOzmen01 <yusufmarquina@gmail.com>
2024-12-07 21:36:56 +0300

    Added a new monthly donation pop up

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,5 @@ import "@fontsource/inter";
 import "./globals.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />
 );
