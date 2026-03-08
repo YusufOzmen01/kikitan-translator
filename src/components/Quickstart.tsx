9b213306a0a3a5b908fe27157294706a1d6f3c40
YusufOzmen01 <yusufmarquina@gmail.com>
2026-03-09 01:57:29 +0300

    Removed default microphone section

--- src/components/Quickstart.tsx
+++ src/components/Quickstart.tsx
@@ -3,7 +3,6 @@ import { Config } from "../util/config"
 
 import {
     GitHub,
-    Settings,
     Translate
 } from '@mui/icons-material';
 
@@ -65,33 +64,7 @@ export default function QuickstartMenu({ config, setLang, lang, setConfig }: Qui
                     </div>
                 </div>
 
-                {/* <div className={'absolute inset-0 transition-all flex justify-center ease-in-out ' + (quickstartPage == 2 ? "opacity-100" : "opacity-0 pointer-events-none")}>
-                    <div className='absolute mt-2 flex flex-col items-center'>
-                        <p className='text-xl bold text-center'>{localization.how_to_setup_and_use_kikitan[lang]}</p>
-                        {quickstartPage == 2 &&
-                            <iframe
-                                width="560"
-                                height="315"
-                                className="mt-4"
-                                src="https://www.youtube-nocookie.com/embed/hB7CDrVnNCs"
-                                // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                                {...({ credentialless: "" } as any)}
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                allowFullScreen
-                            />
-                        }
-                    </div>
-                </div> */}
-
-                <div className={'absolute inset-0 transition-all flex flex-col items-center justify-center ease-in-out ' + (quickstartPage == 2 ? "opacity-100" : "opacity-0 pointer-events-none")}>
-                    <div className='mt-2 mb-4'>
-                        <p className='text-xl bold text-center'>{localization.windows_mic_settings[lang]}</p>
-                        <p className='text-lg mt-20 text-center'>{localization.windows_mic_details[lang]}</p>
-                    </div>
-                    <Button disabled={quickstartPage != 2} className={'w-96 '} variant='contained' startIcon={<Settings />} onClick={() => { invoke("show_audio_settings") }}>{localization.open_win_audio[lang]}</Button>
-                </div>
-
-                <div className={'absolute inset-0 transition-all flex justify-center ease-in-out ' + (quickstartPage == 3 ? "opacity-100" : "opacity-0 pointer-events-none")}>
+                <div className={'absolute inset-0 transition-all flex justify-center ease-in-out ' + (quickstartPage == 2 ? "opacity-100" : "opacity-0 pointer-events-none")}>
                     <div className='absolute mt-2 flex flex-col items-center'>
                         <p className='text-xl bold text-center'>{localization.mode_selection[lang]}</p>
                         <img className='mt-4 w-[256px]' src={
@@ -122,7 +95,7 @@ export default function QuickstartMenu({ config, setLang, lang, setConfig }: Qui
                         color: config.light_mode ? '#666666' : '#4f4f4f'
                     }
                 }} >
-                    <div className={'absolute inset-0 transition-all space-y-2 flex flex-col items-center ease-in-out ' + (quickstartPage == 4 ? "opacity-100" : "opacity-0 pointer-events-none")}>
+                    <div className={'absolute inset-0 transition-all space-y-2 flex flex-col items-center ease-in-out ' + (quickstartPage == 3 ? "opacity-100" : "opacity-0 pointer-events-none")}>
                         <div className='mt-4 mb-4'>
                             <p className='text-4xl bold text-center'>{localization.change_basic_settings[lang]}</p>
                         </div>
@@ -167,29 +140,29 @@ export default function QuickstartMenu({ config, setLang, lang, setConfig }: Qui
                     </div>
                 </Box>
 
-                <div className={'absolute inset-0 transition-all space-y-2 flex flex-col items-center justify-center ease-in-out ' + (quickstartPage == 5 ? "opacity-100" : "opacity-0 pointer-events-none")}>
+                <div className={'absolute inset-0 transition-all space-y-2 flex flex-col items-center justify-center ease-in-out ' + (quickstartPage == 4 ? "opacity-100" : "opacity-0 pointer-events-none")}>
                     <div className='mt-4 mb-4'>
                         <p className='text-xl mt-8 bold text-center'>{localization.thank_you[lang]}</p>
                         <p className='text-lg mt-20 text-center'>{localization.thank_you_details[lang]}</p>
                     </div>
-                    <Button disabled={quickstartPage != 5} className={'w-70'} variant='contained' startIcon={< GitHub />} onClick={async () => { open("https://github.com/YusufOzmen01/kikitan-translator") }}>{localization.open_repo[lang]}</Button>
+                    <Button disabled={quickstartPage != 4} className={'w-70'} variant='contained' startIcon={< GitHub />} onClick={async () => { open("https://github.com/YusufOzmen01/kikitan-translator") }}>{localization.open_repo[lang]}</Button>
                     <div className="flex gap-2">
                         <Button sx={{
                             backgroundColor: "#ffde06",
                             color: "black"
-                        }} disabled={quickstartPage != 5} variant="contained" className='w-52 h-9' onClick={() => { invoke("open_url", { url: "https://buymeacoffee.com/sergiomarquina" }) }}>
+                        }} disabled={quickstartPage != 4} variant="contained" className='w-52 h-9' onClick={() => { invoke("open_url", { url: "https://buymeacoffee.com/sergiomarquina" }) }}>
                             <img src="/buymeacoffeelogo.svg" width={36}></img>
                             <p className="mt-0.5">Buy Me a Coffee</p>
                         </Button>
 
                         <Button sx={{
                             backgroundColor: "#fc4d50"
-                        }} disabled={quickstartPage != 5} variant="contained" className='w-52' onClick={() => { invoke("open_url", { url: "https://booth.pm/en/items/6073050" }) }}>
+                        }} disabled={quickstartPage != 4} variant="contained" className='w-52' onClick={() => { invoke("open_url", { url: "https://booth.pm/en/items/6073050" }) }}>
                             <img src="/boothlogo.svg" width={24} className="mr-2"></img>
                             <p className="mt-0.5">Booth.pm</p>
                         </Button>
                     </div>
-                    <Button disabled={quickstartPage != 5} className={'w-48'} variant='contained' onClick={async () => {
+                    <Button disabled={quickstartPage != 4} className={'w-48'} variant='contained' onClick={async () => {
                         window.localStorage.setItem("firstTimeSetupComplete", "true");
                         localStorage.setItem("lang", lang);
 
@@ -204,12 +177,12 @@ export default function QuickstartMenu({ config, setLang, lang, setConfig }: Qui
                         borderColor: config.light_mode ? '#666666 !important' : '#4f4f4f !important'
                     }
                 }} variant='contained' disabled={quickstartPage == 0} onClick={() => { setQuickstartPage(quickstartPage - 1) }}>{localization.previous[lang]}</Button>
-                {!(quickstartPage == 6) && <Button sx={{
+                {!(quickstartPage == 5) && <Button sx={{
                     '&.Mui-disabled': {
                         color: config.light_mode ? '#666666 !important' : '#4f4f4f !important',
                         borderColor: config.light_mode ? '#666666 !important' : '#4f4f4f !important'
                     }
-                }} className='ml-4' variant='contained' disabled={quickstartPage > 4} onClick={() => { setQuickstartPage(quickstartPage + 1) }}>{localization.next[lang]}</Button>}
+                }} className='ml-4' variant='contained' disabled={quickstartPage > 3} onClick={() => { setQuickstartPage(quickstartPage + 1) }}>{localization.next[lang]}</Button>}
             </div>
         </div>
     </>
